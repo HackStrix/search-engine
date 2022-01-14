7dd942f691446198e1d63e6bf61aadd39dae8659
probro27 <prabhavkhera@hotmail.com>
2022-01-14 11:42:29 -0500

    Server updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const cors = require('cors'); 
-const mongoose = require('mongoose'); 
+// const JSON = require('json')
+// const mongoose = require('mongoose'); 
 
 
 require('dotenv').config(); 
@@ -8,10 +9,12 @@ require('dotenv').config();
 const app= express(); 
 const port = process.env.PORT || 5000; 
 
-const uri = process.env.ATLAS_URI; 
+// const uri = process.env.ATLAS_URI; 
+const uri = "mongodb+srv://admin:password1234$@web-map.qzzvr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const words = ['python']
 
 const MongoClient = require('mongodb').MongoClient;
+const { resourceLimits } = require('worker_threads');
 
 
 
@@ -21,12 +24,63 @@ app.get('/api/search', (req,res)=>{
     const str = req.query.search
     const lst = [str]
     MongoClient.connect(uri, (err,db)=>{
-        if (err) throw err; 
+        if (err) console.log(err); 
+        console.log("we reached here 0")
         var dbo = db.db("web-map");
     
         dbo.collection("tags").find({'word':{'$in':lst}}).toArray((err,result)=>{
+            console.log("we reached here 1")
             if (err) throw err; 
+            let links = []
             console.log(result)
+            console.log(typeof(result))
+            result.map((obj)=>{
+                // let bool;
+                // try{
+                //     JSON.parse(obj)
+                //     console.log(true)
+                // }
+                // catch(e){
+                //     console.log(false)
+                // }
+                for(var m in obj){
+                    // console.log(m);
+                    if(!isNaN(m)){
+                        console.log(obj[m])
+                        obj[m].map((url)=>{
+                            links.push(url)
+                        })
+                    }
+                }
+                
+                // for (var i in result){
+                //     console.log(i)
+                //     if(isNaN(i)){
+                //         for (var j in result[i]){
+                //             links.push(result[i][j])
+                //         }
+                //     }
+                // }
+                // var obj = eval('(' + result + ')');
+                // for(var i in obj){
+                //     console.log(i)
+                //     links.push(obj[i])
+                // }
+            })
+            
+            console.log(links)
+            links.map((url)=>{
+                // console.log(url);
+                var host = url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+                console.log("we reached here 2")
+                console.log(host)
+                const list = [host]
+                dbo.collection("domains").find({'url':{'$in':list}}).toArray((err, result)=>{
+                    console.log(result)
+                })
+                console.log("we reached here 3")
+                // console.log(obj)
+            })
             res.send(result)
             db.close();
         })
