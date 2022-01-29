d539f1703b70883edb3f51993a63c4c4cae1d090
probro27 <prabhavkhera@hotmail.com>
2022-01-28 19:39:31 -0500

    Changed

--- backend.js
+++ backend.js
@@ -70,7 +70,7 @@ catch(e){
 }
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://vision-frontend.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://vision-frontend.herokuapp.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   })
